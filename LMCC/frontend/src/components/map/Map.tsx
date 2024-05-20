e82e4bf8e574136b5b02c89ae275c37d5fb97ed5
Jason Silva <littlejsilva@gmail.com>
2024-05-20 02:15:00 -0400

    hi

--- LMCC/frontend/src/components/map/Map.tsx
+++ LMCC/frontend/src/components/map/Map.tsx
@@ -7,7 +7,7 @@ import GeoIcon from '../../assets/icons/geo.png';
 import HazardImage from '../../assets/icons/hazardImage.png'
 import { v4 as uuidv4 } from 'uuid';
 import './Map.css';
-import React, {useEffect, useRef, useState} from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import MapIcon from "./MapIcon.tsx";
 import GeoPinA from "../../assets/icons/geoPinA.png";
 import GeoPinB from "../../assets/icons/GeoPinB.png";
@@ -23,7 +23,7 @@ function Map() {
   const [poiList, setPoiList] = useState<JSX.Element[]>([]);
   const [hazard,setHazard] = useState<boolean>(false)
   const [hazardList,setHazardList] = useState<JSX.Element[]>([]);
-  const map = useRef<HTMLImageElement>(null)
+
   const vLineWidth = useRef<number>(0)
   const wLineWidth = useRef<number>(0)
   const top = useRef<number>(0);
@@ -31,38 +31,42 @@ function Map() {
   const left = useRef<number>(0);
   const right = useRef<number>(0);
   const [geoMapMarkers,setGeoMapMarkers] = useState<JSX.Element[]>([]);
+  const [hasLoaded,setHasLoaded] = useState(false);
+  const [timer,setTimer] = useState(1);
+  const map = useRef<HTMLImageElement | null>(null)
 
-  useEffect(()=>{
-    const rect = map.current?.getBoundingClientRect();
-    if(!rect) return;
-    top.current = 1.0125 * rect?.top
-    bottom.current = .9875 * rect?.bottom
-    left.current = 1.0125 * rect?.left
-    right.current = .9875 * rect?.right
-    const lrDist = right.current - left.current;
-    const tbDist = bottom.current - top.current;
-    vLineWidth.current = (lrDist / 26.71) * .925;
-    wLineWidth.current = (tbDist / 25.87) * .925;
-
-    console.log(vLineWidth.current);
-    console.log(wLineWidth.current);
-    console.log(bottom.current);
-    setGeoMapMarkers([
-      <img src={GeoPinA} alt={"get a location pin"} className={`absolute h-20 w-12] hover:-translate-y-1 hover:scale-[1.15]`} style={{left:5.85*vLineWidth.current, bottom:7.5*wLineWidth.current}} />,
-      <img src={GeoPinB} alt={"geo b location pin"} className={'absolute h-20 w-12 hover:-translate-y-1 hover:scale-[1.15]'} style={{left:9.9*vLineWidth.current, bottom:9.5*wLineWidth.current}}/>,
-      <img src={GeoPinC} alt={"geo c location pin"} className={'absolute h-20 w-12 bottom-[17.15rem] left-[11.26rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:7.9*vLineWidth.current, bottom:10.65*wLineWidth.current}} />,
-      <img src={GeoPinD} alt={"geo d location pin"} className={'absolute h-20 w-12 bottom-[20.43rem] left-[14.48rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:9.9*vLineWidth.current, bottom:12.6*wLineWidth.current}}/>,
-      <img src={GeoPinE} alt={"geo e location pin"} className={'absolute h-20 w-12 bottom-[18.8rem] left-[21.06rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:14.9*vLineWidth.current, bottom:11.6*wLineWidth.current}}/>,
-      <img src={GeoPinF} alt={"geo f location pin"} className={'absolute h-20 w-12 bottom-[25.35rem] left-[29.25rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:18.9*vLineWidth.current, bottom:15.6*wLineWidth.current}}/>,
-      <img src={GeoPinG} alt={"geo g location pin"} className={'absolute h-20 w-12 bottom-[25.35rem] left-[21.06rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:14.9*vLineWidth.current, bottom:15.6*wLineWidth.current}}/>,
-      <img src={UiaPin} alt={"Uia pin location"} className={'absolute h-20 w-16 bottom-[8.85rem] left-[22.25rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:15.6*vLineWidth.current, bottom:6.5*wLineWidth.current}}/>,
-      <img src={CommPin} alt={"Comm pin location"} className={'absolute h-20 w-20 bottom-[23.7rem] left-[31.5rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:19.25*vLineWidth.current, bottom:13.7*wLineWidth.current}}/>
-    ])
+    useEffect(()=>{
+        const l = new Promise(r => setTimeout(r, 1000)).then(()=>{
+        const rect = map.current?.getBoundingClientRect();
 
 
 
+        top.current = 1.0125 * rect.top
+        bottom.current = .9875 * rect.bottom
+        left.current = 1.0125 * rect.left
+        right.current = .9875 * rect.right
+        const lrDist = right.current - left.current;
+        const tbDist = bottom.current - top.current;
+        vLineWidth.current = (lrDist / 26.71) * .925;
+        wLineWidth.current = (tbDist / 25.87) * .925;
 
-  },[map])
+        setGeoMapMarkers([
+          <img key={uuidv4()} src={GeoPinA} alt={"get a location pin"} className={`absolute h-20 w-12] hover:-translate-y-1 hover:scale-[1.15]`} style={{left:5.85*vLineWidth.current, bottom:7.5*wLineWidth.current}} />,
+          <img key={uuidv4()} src={GeoPinB} alt={"geo b location pin"} className={'absolute h-20 w-12 hover:-translate-y-1 hover:scale-[1.15]'} style={{left:9.9*vLineWidth.current, bottom:9.5*wLineWidth.current}}/>,
+          <img key={uuidv4()} src={GeoPinC} alt={"geo c location pin"} className={'absolute h-20 w-12 bottom-[17.15rem] left-[11.26rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:7.9*vLineWidth.current, bottom:10.65*wLineWidth.current}} />,
+          <img key={uuidv4()} src={GeoPinD} alt={"geo d location pin"} className={'absolute h-20 w-12 bottom-[20.43rem] left-[14.48rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:9.9*vLineWidth.current, bottom:12.6*wLineWidth.current}}/>,
+          <img key={uuidv4()} src={GeoPinE} alt={"geo e location pin"} className={'absolute h-20 w-12 bottom-[18.8rem] left-[21.06rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:14.9*vLineWidth.current, bottom:11.6*wLineWidth.current}}/>,
+          <img key={uuidv4()} src={GeoPinF} alt={"geo f location pin"} className={'absolute h-20 w-12 bottom-[25.35rem] left-[29.25rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:18.9*vLineWidth.current, bottom:15.6*wLineWidth.current}}/>,
+          <img key={uuidv4()} src={GeoPinG} alt={"geo g location pin"} className={'absolute h-20 w-12 bottom-[25.35rem] left-[21.06rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:14.9*vLineWidth.current, bottom:15.6*wLineWidth.current}}/>,
+          <img key={uuidv4()} src={UiaPin} alt={"Uia pin location"} className={'absolute h-20 w-16 bottom-[8.85rem] left-[22.25rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:15.6*vLineWidth.current, bottom:6.5*wLineWidth.current}}/>,
+          <img key={uuidv4()} src={CommPin} alt={"Comm pin location"} className={'absolute h-20 w-20 bottom-[23.7rem] left-[31.5rem] hover:-translate-y-1 hover:scale-[1.15]'} style={{left:19.25*vLineWidth.current, bottom:13.7*wLineWidth.current}}/>
+        ])
+      });
+
+    },[])
+
+
+
 
   const placeIcon = (e:React.MouseEvent<HTMLImageElement, MouseEvent>)=>{
     if(hazard){
@@ -93,6 +97,7 @@ function Map() {
         onClick={(e:React.MouseEvent<HTMLImageElement, MouseEvent>)=>placeIcon(e)}
         ref={map}
 
+
       />
       {geoMapMarkers}
 
@@ -106,6 +111,10 @@ function Map() {
             src={HistoryIcon}
             alt='Show History'
             className='map-button-image'
+            onLoad={()=>{
+              if(!hasLoaded) {
+                setHasLoaded(true)
+              }}}
           />
           <span>Show History</span>
         </button>
